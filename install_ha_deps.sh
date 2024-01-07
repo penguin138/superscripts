929cb8bb9c04b4c5bd8e197a5bd3f6372667a735
Nastya Torunova <torunova@phystech.edu>
2024-01-07 17:24:50 +0000

    add home assistant dependencies installation script

---
+++ install_ha_deps.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+apt install \
+apparmor \
+cifs-utils \
+curl \
+dbus \
+jq \
+libglib2.0-bin \
+lsb-release \
+network-manager \
+nfs-common \
+systemd-journal-remote \
+systemd-resolved \
+udisks2 \
+wget -y
