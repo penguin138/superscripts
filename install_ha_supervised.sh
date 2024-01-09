258d595e70ca8c6032adac57e2f808f91f388421
Nastya Torunova <torunova@phystech.edu>
2024-01-09 22:51:26 +0000

    fix ha installation on unsupported os

--- install_ha_supervised.sh
+++ install_ha_supervised.sh
@@ -3,4 +3,4 @@
 version="1.4.3"
 
 wget -O homeassistant-supervised.deb https://github.com/home-assistant/supervised-installer/releases/download/${version}/homeassistant-supervised.deb
-sudo dpkg -i ./homeassistant-supervised.deb
+sudo env "BYPASS_OS_CHECK=true" dpkg -i ./homeassistant-supervised.deb
