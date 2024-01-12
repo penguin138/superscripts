9c791e473fe61d94091e1a0bbe5ae7cc5b490abd
penguin138 <nastya@torunova.com>
2024-01-12 19:43:24 +0000

    update last installation step

--- install_ha_supervised.sh
+++ install_ha_supervised.sh
@@ -3,4 +3,6 @@
 version="1.4.3"
 
 wget -O homeassistant-supervised.deb https://github.com/home-assistant/supervised-installer/releases/download/${version}/homeassistant-supervised.deb
+echo "systemd.unified_cgroup_hierarchy=0" >> /boot/cmdline.txt
+echo "apparmor=1 security=apparmor" >> /boot/cmdline.txt
 sudo env "BYPASS_OS_CHECK=true" dpkg -i ./homeassistant-supervised.deb
